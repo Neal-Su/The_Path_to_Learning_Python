7130dce61fd8e0f5fcd337b16e4b37b2bd32c899
Neal-Su <m18842324039@163.com>
2024-08-21 19:20:07 +0800

    布尔类型和比较运算符

---
+++ Chapter One/Unit2-01.py
@@ -0,0 +1,17 @@
+#bool_1 = True
+#bool_2 = False
+#print(f"bool_1变量的内容是：{bool_1},类型是：{type(bool_1)}")
+#print(f"bool_1变量的内容是：{bool_2},类型是：{type(bool_2)}")
+num1 = 10
+num2 = 15
+print(f"10!=15的结果是：{num1 != num2}")
+name1 = "itcast"
+name2 = "itheima"
+print(f"itcast == itheima结果是：{name1 == name2}")
+num1 = 10
+num2 = 5
+print(f"10>5的结果是：{num1 > num2}")
+print(f"10<5的结果是：{num1 < num2}")
+num2 = 11
+print(f"10>=11的结果是：{num1 >= num2}")
+print(f"10<=11的结果是：{num1 <= num2}")
