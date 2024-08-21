fdd341083e0f5f0ab25e0519551c1abb5adc509f
Neal-Su <m18842324039@163.com>
2024-08-21 19:08:09 +0800

    input数据输入

---
+++ Chapter One/Unit1-03.py
@@ -0,0 +1,5 @@
+#name = input("请告诉我你是谁？")
+#print("我知道了：你是：%s" % name)
+num = input("请告诉我你的银行卡密码：")
+num = int(num)
+print("你的银行卡密码类型是：",type(num))
---
+++ Chapter One/Unit1-04.py
@@ -0,0 +1,3 @@
+user_name = input("请输入您的用户名：")
+user_type = "SSSVIP"
+print(f"您好：{user_name},您是尊贵的：{user_type}用户，欢迎您的登录。")
