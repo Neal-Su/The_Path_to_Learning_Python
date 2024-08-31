be1a5d7dcafe9824e71c65605769617ac0c9018d
Neal-Su <m18842324039@163.com>
2024-08-31 15:34:27 +0800

    for循环遍历

---
+++ Chapter One/Unit2-14.py
@@ -0,0 +1,10 @@
+#name = "ithema"
+#for x in name:
+#    print(x)
+name = "itheiman is a brand of itcast"
+i = 0
+for x in name:
+    if x == "a":
+        i += 1
+print(f"{name}中共含有：{i}个字母a")
+# for循环遍历
