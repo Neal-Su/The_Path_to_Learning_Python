32dc17378e0aa8bed8873d35266fabed2dd31868
Neal-Su <m18842324039@163.com>
2024-08-31 15:35:00 +0800

    range序列语句

---
+++ Chapter One/Unit2-15.py
@@ -0,0 +1,9 @@
+#for x in range(10):
+#    print(x)
+# for x in range(5,10):
+#     print(x)
+# for x in range(5,10,2):
+#     print(x)
+for x in range(5,10,2):
+    print("送玫瑰花")
+#  range序列语句
---
+++ Chapter One/Unit2-16.py
@@ -0,0 +1,7 @@
+num = 100
+i = 0
+for x in range(1,num):
+    if x % 2 == 0:
+        i += 1
+print(f"1到100（不含100）范围内，有{i}个偶数")
+#     range序列语句
