5c0ae5147eddf4ed472241e963640b46fcef2504
Neal-Su <m18842324039@163.com>
2024-09-02 22:21:28 +0800

    # 认识函数

---
+++ Chapter One/Unit4-01.py
@@ -0,0 +1,12 @@
+str1 ="vaqowurpn"
+str2 ="vaure"
+str3 ="asdfuiu"
+def my_len(data):
+    count = 0
+    for i in data:
+        count += 1
+    print(f"字符串{data}的长度是：{count}")
+my_len(str1)
+my_len(str2)
+my_len(str3)
+# 认识函数
