65339f8772b177497642d98c6b07996d9620c2f2
Neal-Su <m18842324039@163.com>
2024-09-03 16:44:15 +0800

    函数说明文档

---
+++ Chapter One/Unit4-09.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    """
+    add函数可以接受2个参数，进行两数相加的功能
+    :param x: 形参x表示相加的其中一个数字
+    :param y: 形参y表示相加的另一个数字
+    :return: 返回值是两数相加的结果
+    """
+    result = x + y
+    print(f"两数相加的结果是：{result}")
+    return result
+add(1,2)
+# 函数说明文档
