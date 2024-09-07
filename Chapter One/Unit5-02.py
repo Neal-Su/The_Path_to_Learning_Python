da75d445ba417927b1c08ef9263e3c5b0a2d6250
Neal-Su <m18842324039@163.com>
2024-09-07 11:04:12 +0800

    列表的下标索引

---
+++ Chapter One/Unit5-02.py
@@ -0,0 +1,12 @@
+# my_list = ["Tom","Lily","Rose"]
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[-1])
+# print(my_list[-2])
+# print(my_list[-3])
+
+my_list = [[1,2,3],[4,5,6]]
+print(my_list[1][1])
+
+# 列表的下标索引
