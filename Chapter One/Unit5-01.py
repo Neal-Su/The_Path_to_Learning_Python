712ab73d1c050f570bb68fabc02aceacd10c567f
Neal-Su <m18842324039@163.com>
2024-09-07 11:03:51 +0800

    列表的定义

---
+++ Chapter One/Unit5-01.py
@@ -0,0 +1,15 @@
+# my_list = ["itheima","itcast","python"]
+# print(my_list)
+# print(type(my_list))
+
+# my_list = ["itheima",666,True]
+# print(my_list)
+# print(type(my_list))
+
+my_list = [[1,2,3],[4,5,6]]
+print(my_list)
+print(type(my_list))
+
+# 列表可以一次存储多个数据，且可以为不同的数据类型，支持嵌套
+
+# 列表的定义
