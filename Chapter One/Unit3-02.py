cd39f7eed90cd3fc19e5058da77e0d83d910a292
Neal-Su <m18842324039@163.com>
2024-09-18 15:03:35 +0800

    制表符对齐

--- Chapter One/Unit3-02.py
+++ Chapter One/Unit3-02.py
@@ -1,2 +1,8 @@
 print("Hello\tWorld")
 print("itheima\tbest")
+
+print(f"列表 \t元素个数有：{len(my_list)}个")
+print(f"元组 \t元素个数有：{len(my_list)}个")
+print(f"字符串 \t元素个数有：{len(my_list)}个")
+print(f"集合 \t元素个数有：{len(my_list)}个")
+print(f"字典 \t元素个数有：{len(my_list)}个")
