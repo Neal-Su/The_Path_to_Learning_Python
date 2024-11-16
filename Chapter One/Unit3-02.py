6f6af841195ea3d7eb141aa93d16a7f1f5a5284a
Neal-Su <m18842324039@163.com>
2024-11-16 22:16:15 +0800

    制表符对齐

--- Chapter One/Unit3-02.py
+++ Chapter One/Unit3-02.py
@@ -6,3 +6,7 @@ print(f"元组 \t元素个数有：{len(my_list)}个")
 print(f"字符串 \t元素个数有：{len(my_list)}个")
 print(f"集合 \t元素个数有：{len(my_list)}个")
 print(f"字典 \t元素个数有：{len(my_list)}个")
+
+# 输出结果
+Hello	World
+itheima	best
