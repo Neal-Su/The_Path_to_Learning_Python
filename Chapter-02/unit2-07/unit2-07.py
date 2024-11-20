a5d8403f72e489817581909eacd984802f1a14c0
Neal-Su <m18842324039@163.com>
2024-11-20 23:02:58 +0800

    数据库综合案例

--- Chapter-02/unit2-07/unit2-07.py
+++ Chapter-02/unit2-07/unit2-07.py
@@ -39,7 +39,7 @@ conn = Connection(
     host="localhost",  # (主机名)，ip地址也可以，localhost表示自己的电脑
     port=3306,  # 端口号，默认就是3306
     user="root",  # 账户，默认是root，这是mysql数据库的管理员账户
-    password="mysql465899",  # mysql数据库密码，这里因为上传github，密码不是正确的，需要使用记得查询正确密码
+    password="mysql",  # mysql数据库密码，这里因为上传github，密码不是正确的，需要使用记得查询正确密码
     autocommit=True  # 设置自动提交
 )
 # 获取游标对象
